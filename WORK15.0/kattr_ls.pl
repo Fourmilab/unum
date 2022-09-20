6ba7be889e0cffcaf3beb250fe6f1ac483e0257b
John Walker <kelvin@fourmilab.ch>
2022-09-20 14:01:42 +0200

    Version 3.5-15.0.0 release, incorporating Unicode 15.0.0

---
+++ WORK15.0/kattr_ls.pl
@@ -0,0 +1,61 @@
+
+    #	Show example of "k" attributes
+
+    use strict;
+    use warnings;
+    
+    
+    use XML::Parser;
+    use Data::Dumper;
+    
+    binmode(STDOUT, ":utf8");
+    
+    my $parser = new XML::Parser(
+    	    Handlers => {
+	    	Start => \&p_start,
+		End => \&p_end
+		},
+	    ProtocolEncoding => 'UTF-8'
+    	);
+	
+    $parser->parsefile('ucd.all.flat.xml');
+    
+    my @e_stack;
+    
+    my (%ka, %kn);
+    
+    sub p_start {
+    	my ($expat, $element, %attrs) = @_;
+	
+	my $line = $expat->current_line;
+#	print("Start $element on line $line\n");
+	push(@e_stack, { element=>$element, line=>$line });
+	
+	if (%attrs) {
+#	    print("Attributes: \n");
+#	    while (my($key, $value) = each(%attrs)) {
+#	    	print("    $key => $value\n");
+#	    }
+
+    	    while (my($key, $value) = each(%attrs)) {
+	    	if ($key =~ m/^k/) {
+		    if ($value ne '') {
+		    	printf("%-20s   %s\n", $key, $value);
+			$ka{$key} = $value;
+			$kn{$key}++;
+		    }
+		}
+	    }
+    	}
+    }
+    
+    sub p_end {
+    	my ($expat, $element) = @_;
+	
+	my $element_record = pop(@e_stack);
+	
+#	print("End $element which started on line ", $$element_record{line},
+#	    "\n");
+    }
+    
+#    print(Dumper($chars));
